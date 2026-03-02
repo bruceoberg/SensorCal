f751e8a998deb4e6ff2eece5f8032fa6606fbc3f
bruce oberg <bruce@oberg.org>
2026-03-02 14:29:54 -0800

    move from extern (subtrees) to vendor (submodules)

--- extern/image2wx/image2wx-config.cmake
+++
@@ -1,7 +0,0 @@
-get_filename_component(image2wx_SOURCE_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-
-set(image2wx_USE_FILE "${image2wx_SOURCE_DIR}/image2wx-use.cmake")
-set(image2wx_INCLUDE_DIRS "${image2wx_SOURCE_DIR}/include")
-
-set(image2wx_BIN2C_FILE "${image2wx_SOURCE_DIR}/image2wx-bin2c.cmake")
-set(image2wx_IMAGE_C_TEMPLATE_FILE "${image2wx_SOURCE_DIR}/image.cpp.in")
--- extern/image2wx/include/image2wx/image2wx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <wx/bitmap.h>
-
-namespace image2wx
-{
-	wxBitmap BitmapFromName(const char *name);
-	void FreeBitmaps();
-} // namespace image2wx
