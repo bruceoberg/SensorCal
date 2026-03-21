0540b007806219a404c6f6cd0fa6b2da5fbb2fa8
bruce oberg <bruce@oberg.org>
2026-03-21 12:36:53 -0700

    use glm as a pegged submodule instead of as a nix package.

--- shell.nix
+++ shell.nix
@@ -4,15 +4,20 @@ pkgs.mkShell
 {
   packages =
   [
+    # tools needed to build
     pkgs.cmake
     pkgs.clang
     pkgs.ninja        # the build system cmake will use (ninja is effectively make++)
+    # libraries needed to build
     pkgs.wxGTK32      # wxwidgets
-    pkgs.glm          # OpenGL math
+    # nix specific tools
     pkgs.pkg-config   # let's cmake find installed libs (eg wx)
+    # utilities
     pkgs.claude-code
     pkgs.imagemagick
   ];
+
+  # ensure we get clang from nix
 
   shellHook = ''
     export CC=${pkgs.clang}/bin/clang
